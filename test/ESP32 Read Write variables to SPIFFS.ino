b9f622e68d3b2055d9738ecddfc6463154b37d83
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2020-12-09 09:48:13 +0100

    Create ESP32 Read Write variables to SPIFFS.ino

---
+++ test/ESP32 Read Write variables to SPIFFS.ino
@@ -0,0 +1,5 @@
+// ESP32 Read Write variables to SPIFFS
+// Working on it
+//
+// https://oshwlab.com/l.dijkman/esp32-dev-kit-38-pin-to-spi-touch-tft
+// https://github.com/ldijkman/ART-ESP32-Touch-TFT-Thermostat
