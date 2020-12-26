2173c9a3fcc166c6c85934466aaa51a961b1b4c9
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2020-12-26 17:41:03 +0100

    Update Arts_Memory.ino

--- Arts_Memory.ino
+++ Arts_Memory.ino
@@ -23,31 +23,7 @@
 // total time cooling on
 //
 //
-//
-//
-//
-// Wouldnt that be Great if Art can remember Things
-// Read from Arts memory and write into Arts memory
-//
-// touch calibrate setting save has already formatted Arts Brain if needed
-//
-// Use new brain-file for Art => file = "/Arts_Memory";
-//
-//***************************
-// what do we want to save
-//***************************
-// eco setpoint
-// normal setpoint
-// cool setpoint
-// switchpoint above / below
-// alarm setpoints low/high
-// screen brightnes
-//
-// auto mode  program & times temps
-//
-// Arts age ;-) time since startdate / memory creation?
-//
-//
+
 //
 //
 //
