bd1315714d6472984b0b122ac0d75f51614c25be
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2020-12-26 09:31:35 +0100

    Update Arts_Memory.ino

--- Arts_Memory.ino
+++ Arts_Memory.ino
@@ -4,3 +4,16 @@
 // touch calibrate setting save has already formatted Arts Brain if needed 
 //
 // Use new brain-file for Art => file = "/Arts_Memory"; 
+//
+//***************************
+// what do we want to save
+//***************************
+// eco setpoint
+// normal setpoint
+// cool setpoint
+// switchpoint above / below
+// alarm setpoints low/high
+// screen brightnes
+//
+// auto mode  program & times temps
+
