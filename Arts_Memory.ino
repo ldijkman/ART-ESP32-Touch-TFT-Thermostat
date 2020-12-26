494330d2d56c9768a69810f39c2fe7a321c97903
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2020-12-26 09:20:50 +0100

    Update Arts_Memory.ino

--- Arts_Memory.ino
+++ Arts_Memory.ino
@@ -1,2 +1,6 @@
 // Wouldnt that be Great if Art can remember Things
 // Read from Arts memory and write into Arts memory
+// 
+// touch calibrate setting save has already formatted Arts Brain if needed 
+//
+// Use another brain-file for Art => file = "/Arts_Memory" 
