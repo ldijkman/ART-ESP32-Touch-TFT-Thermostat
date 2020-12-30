e2e3cff3bf9d99536b1039be479c666250d98ce0
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2020-12-30 17:24:51 +0100

    Update ESP32_BME280_barometer_humidity_temperature_sensor_test.ino

--- test/ESP32_BME280_barometer_humidity_temperature_sensor_test.ino
+++ test/ESP32_BME280_barometer_humidity_temperature_sensor_test.ino
@@ -8,8 +8,9 @@ For Art Thermostat, a better NEST
 BME 280 Baromter Humidity and temperature sensor
 ESP32 arduino ide example
 
-https://oshwlab.com/l.dijkman
-https://github.com/ldijkman/ART-ESP32-Touch-TFT-Thermostat
+// https://oshwlab.com/l.dijkman/esp32-dev-kit-38-pin-to-spi-touch-tft 
+// https://github.com/ldijkman/ART-ESP32-Touch-TFT-Thermostat 
+// http://www.Arduino.TK
 
 i2c SDA SCL 
 gpio 21, gpio 22
