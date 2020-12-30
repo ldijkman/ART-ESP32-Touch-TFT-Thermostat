6591ce33e2e8dbba092d2cff232e6c4ddff39f7e
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2020-12-30 17:22:03 +0100

    Update ESP32 LDR light sensor analog input.ino

--- test/ESP32 LDR light sensor analog input.ino
+++ test/ESP32 LDR light sensor analog input.ino
@@ -1,6 +1,7 @@
 // ESP32 LDR test should work 
-// https://oshwlab.com/l.dijkman/esp32-dev-kit-38-pin-to-spi-touch-tft
-// https://github.com/ldijkman/ART-ESP32-Touch-TFT-Thermostat
+// https://oshwlab.com/l.dijkman/esp32-dev-kit-38-pin-to-spi-touch-tft 
+// https://github.com/ldijkman/ART-ESP32-Touch-TFT-Thermostat 
+// http://www.Arduino.TK
 
 int LDRValue=0;
 byte LIGHT_SENSOR = 36; //  GPIO 36 Analog Input
