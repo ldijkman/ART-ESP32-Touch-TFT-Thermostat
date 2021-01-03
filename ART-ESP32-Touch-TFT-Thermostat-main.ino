7f21868751edbdfa5e316c2673d25754f564e473
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-01-03 06:57:53 +0100

    Update ART-ESP32-Touch-TFT-Thermostat-main.ino

--- ART-ESP32-Touch-TFT-Thermostat-main.ino
+++ ART-ESP32-Touch-TFT-Thermostat-main.ino
@@ -3,7 +3,10 @@
 // https://github.com/ldijkman/ART-ESP32-Touch-TFT-Thermostat 
 // http://www.Arduino.TK
 //
-//
+// GNU General Public License, 
+// which basically means that you may freely copy, change, and distribute it, 
+// but you may not impose any restrictions on further distribution, 
+// and you must make the source code available.
 //
 //
 //
