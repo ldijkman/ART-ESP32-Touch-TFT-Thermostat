7fe57bcafee6b3c5eae0f1866a317ab5662b8142
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-10-23 08:15:56 +0200

    Update ESP8266_MDNS_Hostname_relais_switch_list_other_on_webpage.ino.ino

--- test/art_test_1/Remote_WiFi_Sense_Switch/ESP8266_MDNS_Hostname_relais_switch_list_other_on_webpage.ino.ino
+++ test/art_test_1/Remote_WiFi_Sense_Switch/ESP8266_MDNS_Hostname_relais_switch_list_other_on_webpage.ino.ino
@@ -1,5 +1,5 @@
 // esp8266 relais switch config portal mdns http//?????.local hostnames
-// i noticed that my sonoff are ESP8285 bt it programs well wth => generic ESP8266 settings of arduino ide
+// i noticed that my sonoffs are ESP8285 but it programs well with => generic ESP8266 settings of arduino ide
 //
 // https://www.youtube.com/watch?v=sRqMcyHR-Ls
 //
