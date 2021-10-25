88a2f83a3bdfa86dbc53828119a56f16bf1aaa1d
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-10-25 18:17:30 +0200

    Update ESP8266_MDNS_Hostname_relais_switch_list_other_on_webpage.ino.ino

--- test/art_test_1/Remote_WiFi_Sense_Switch/ESP8266_MDNS_Hostname_relais_switch_list_other_on_webpage.ino.ino
+++ test/art_test_1/Remote_WiFi_Sense_Switch/ESP8266_MDNS_Hostname_relais_switch_list_other_on_webpage.ino.ino
@@ -1,5 +1,8 @@
 // I Do Not like that Google Fucked Up the Google login on Raspberry Pi Chromium Browser!!! => sudo apt install -y chromium-browser rpi-chromium-mods
 //
+// https://github.com/ldijkman/ART-ESP32-Touch-TFT-Thermostat/tree/main/test/art_test_1/Remote_WiFi_Sense_Switch
+/
+//
 // esp8266 relais switch tzapu wifi manager config portal with mdns http//?????.local hostnames
 // i noticed that my sonoffs are ESP8285 but it programs well with => generic ESP8266 settings of arduino ide
 // tested with sonoff basic esp8285, esp01-esp8266, wemos d1 mini esp8266 with relais shield
